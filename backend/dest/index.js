aae4c9edf8c5238ebe506797a10b303939fb4702
sobhi69 <mhegazy6371@gmail.com>
2025-05-09 16:09:29 +0300

    commit2

---
+++ backend/dest/index.js
@@ -0,0 +1,22 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const cors_1 = __importDefault(require("cors"));
+const port = 3006;
+const user_1 = __importDefault(require("./routes/user"));
+app.get('/', (req, res) => {
+    res.json({ message: "Thanks God           1" });
+});
+app.use((0, cors_1.default)({
+    credentials: true,
+    origin: '',
+    optionsSuccessStatus: 200
+}));
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ 'extended': false }));
+app.use('/api/user', user_1.default);
+app.listen(port, () => console.log(`app is alive at http://localhost:${port}`));
