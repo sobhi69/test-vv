55772e3c25dd7299ae3b64a8d736fa4d06b1a1c6
Mohamed sobhi <116953847+sobhi69@users.noreply.github.com>
2025-05-09 16:17:55 +0300

    Update axiosInstance.ts

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    "baseURL":"https://test-vv-backend-1wokbmihn-sobhi69s-projects.vercel.app/api"
+    "baseURL":"https://test-vv-backend-1wokbmihn-sobhi69s-projects.vercel.app/api",
+    withCredentials:true
 })
 
 export default axiosInstance
